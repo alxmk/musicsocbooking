e0de03635e0f0fb1ce843369d5a16f038c16e574
Alex Kelly <alex.kelly@william-lee.com>
2011-10-17 10:20:00 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Musicsocbooking::Application.config.secret_token = '1ff79edc9dffffc0d0d4f9daa34b3c6b50afa3408bd4b0b7c1e47ff08076ad757d99f651efb950752ce70225a7244ac74cbcb483671a1f9f13b08d3ff12de275'
